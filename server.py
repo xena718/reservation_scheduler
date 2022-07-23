e88f5871c32973bb6a73685f37b41583f0ee6dad
Chengfeng Ren <chengfengren@gmail.com>
2022-07-23 04:33:46 +0000

    update

--- server.py
+++ server.py
@@ -8,3 +8,15 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+
+@app.route("/")
+def homepage():
+    """view homepage"""
+
+    return render_template("homepage.html")
+
+
+
+if __name__ == "__main__":
+    connect_to_db(app) # connect to your database before app.run gets called, so that Flask can access your database.
+    app.run(host="0.0.0.0", debug=True)
---
+++ static/events.js
@@ -0,0 +1,3 @@
+
+document.getElementById('datePicker').valueAsDate = new Date();
+
