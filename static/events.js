3df1c41d91be18a8ec2a1ee976e7a0fb2b09e103
Chengfeng Ren <chengfengren@gmail.com>
2022-07-23 05:27:33 +0000

    update homepage

--- static/events.js
+++ static/events.js
@@ -1,3 +1,26 @@
 
 document.getElementById('datePicker').valueAsDate = new Date();
 
+
+function createOption(value, text) {
+    const option = document.createElement('option');
+    option.text = text;
+    option.value = value;
+    return option;
+}
+
+const hourSelectAll = document.querySelectorAll('.hours');
+for (const hourSelect of hourSelectAll) {
+
+for(let i = 0; i <= 12; i++){
+        hourSelect.add(createOption(i, i));
+}
+}
+
+const minutesSelectAll = document.querySelectorAll('.minutes');
+for(const minutesSelect of minutesSelectAll){
+for(let i = 0; i < 60; i += 30) {
+        minutesSelect.add(createOption(i, i));
+}
+}
+
